db02cdc2591273c5273c6efe8a6d4abcfcf309bd
Oleksii Moiseenko <>
2019-08-01 00:55:29 +0300

    appended utils;

---
+++ utils/utils.go
@@ -0,0 +1,44 @@
+package shared
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"github.com/ttacon/libphonenumber"
+	"regexp"
+)
+
+func GetMD5Hash(text string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+type Equatable interface {
+	isEqual(another interface{}) bool
+}
+
+type Hashable interface {
+	GetHash() string
+}
+
+func IsValidPhone(value string) bool {
+	_, err := libphonenumber.Parse(value, "")
+	return err == nil
+}
+
+func IsValidEmail(value string) bool {
+	re := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+	return re.MatchString(value)
+}
+
+func IsEqualArray(a, b []Equatable) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v.isEqual(b[i]) {
+			return false
+		}
+	}
+	return true
+}
